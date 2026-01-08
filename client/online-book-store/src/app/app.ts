d198301eb227283f80c8efda9aabc3bd41e790a7
labuser <labuser@ip-172-31-15-211.ap-south-1.compute.internal>
2026-01-08 06:41:29 +0000

    added wishlist

--- client/online-book-store/src/app/app.ts
+++ client/online-book-store/src/app/app.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './auth/components/login/login';
 import { RegisterComponent } from './auth/components/register/register';
 import { DashboardComponent } from "./dashboard/components/user-dashboard/user-dashboard";
+import { WishlistComponent } from "./wishlist/wishlist";
+import { CartComponent } from "./cart/cart";
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, RegisterComponent, DashboardComponent],
+  imports: [WishlistComponent],  //LoginComponent, RegisterComponent, DashboardComponent,, CartComponent
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
